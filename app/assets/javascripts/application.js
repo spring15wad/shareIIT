c70196f44947197a2a80c240518728d4270e0954
Neeraj Soparawala <nsoparaw@hawk.iit.edu>
2015-04-24 18:22:54 -0500

    added nav bar and mobile nav bar styling along with jquery toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,57 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+        $("#howitworkscontent").hide();
+        $("#signincontent").hide();
+        $("#abtuscontent").show();
+
+    $("#abtus").click(function(){
+        $("#howitworkscontent").hide();
+        $("#signincontent").hide();
+        $("#abtuscontent").show();
+    });
+    $("#howitworks").click(function(){
+        $("#abtuscontent").hide();
+        $("#signincontent").hide();
+        $("#howitworkscontent").show();
+    });
+    $("#signin").click(function(){
+        $("#abtuscontent").hide();
+        $("#howitworkscontent").hide();
+        $("#signincontent").show();
+    });
+
+    // Create mobile element
+ var mobile = document.createElement('div');
+ mobile.className = 'nav-mobile';
+ document.querySelector('.nav').appendChild(mobile);
+
+ // hasClass
+ function hasClass(elem, className) {
+ return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+ }
+
+ // toggleClass
+ function toggleClass(elem, className) {
+ var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+ if (hasClass(elem, className)) {
+ while (newClass.indexOf(' ' + className + ' ') >= 0) {
+ newClass = newClass.replace(' ' + className + ' ', ' ');
+ }
+ elem.className = newClass.replace(/^\s+|\s+$/g, '');
+ } else {
+ elem.className += ' ' + className;
+ }
+ }
+
+ // Mobile nav function
+ var mobileNav = document.querySelector('.nav-mobile');
+ var toggle = document.querySelector('.navlist');
+ mobileNav.onclick = function () {
+ toggleClass(this, 'nav-mobile-open');
+ toggleClass(toggle, 'nav-active');
+ };
+
+});
