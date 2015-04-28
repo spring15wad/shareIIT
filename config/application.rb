af0beb8c8d6b34e37dcb399d4c5267b734980d97
Neeraj Soparawala <nsoparaw@hawk.iit.edu>
2015-04-28 02:59:59 -0500

    still trying1

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module ShareIITTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    Bundler.require(*Rails.groups(:assets => %w(development production test)))
+    Bundler.require(*Rails.groups(:assets => %w(development test)))
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -28,6 +28,8 @@ module ShareIITTest
 
 
     config.assets.initialize_on_precompile = false
+    config.assets.version = '1.0'
+
 
   end
 end
