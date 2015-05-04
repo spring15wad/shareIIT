8adf01c7e203e64280ec8a4bc6bd2e0762777e88
M.J. Freeman <mfreema5@hawk.iit.edu>
2015-05-04 11:21:05 -0500

    AFAICT, class name needs to match filename.

--- db/migrate/20150421043628_add_requesttype_to_requests.rb
+++ db/migrate/20150421043628_add_requesttype_to_requests.rb
@@ -1,4 +1,6 @@
-class AddTypeToRequests < ActiveRecord::Migration
+#20150421043628_add_requesttype_to_requests.rb
+#class AddTypeToRequests < ActiveRecord::Migration  <-- Class name doesn't match filename
+class AddRequesttypeToRequests < ActiveRecord::Migration
   def change
     add_column :requests, :requesttype, :string
   end
